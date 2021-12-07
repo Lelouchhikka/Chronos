fd9f9b92de98fda1998549af7078061c39e3fab4
Lelouchhikka <erasyl.kelman@gmail.com>
2021-12-07 19:23:16 +0600

    Initial commit

---
+++ src/main/java/com/itstep/trelloanalog/TrelloAnalogApplication.java
@@ -0,0 +1,13 @@
+package com.itstep.trelloanalog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrelloAnalogApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TrelloAnalogApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/itstep/trelloanalog/TrelloAnalogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.itstep.trelloanalog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrelloAnalogApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
